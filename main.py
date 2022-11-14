8e587c75c052c366c4a6e59d1585b3fa2b45cd6f
vishal <vivevich3@gmail.com>
2022-11-14 21:04:36 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+def list1(a):
+    list=[10,20,30,40,50]
+    len(list)
+def lis2(a):
+    list1=list(a)
+    print (list)
+def list3(a):
+    list=[10,20,30,40,50]
+    list=[10,20,30,40]
+    list.append(50)
+    print (list)
+def list5(a):
+    list=[10,20,30,40,50]
+    list2=[40,50]
+    list.extend(list2)
+def list6(a):
+    list=[10,20,30,40,50]
+    list.insert(2,25)
+    print (list)
+def list7 (a):
+    list=[10,20,30,40,50]
+    list.count(10)
+    print (list)
+def list8 (a):
+    list=[10,20,30,40,10]
+    list.index(20)
+    print (list)
+def list9 (a):
+    list=[10,20,30,40,50]
+    
